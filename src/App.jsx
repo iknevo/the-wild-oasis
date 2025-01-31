7e8157b474525c98d7981da155131dfd18a91083
idknevo <nevoexists@gmail.com>
2025-01-31 15:55:38 +0200

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.div`
+  padding: 20px;
+  background-color: orangered;
+`;
+
 export default function App() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => console.log("check in")}>Check in</Button>
+        <Button onClick={() => console.log("check out")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
